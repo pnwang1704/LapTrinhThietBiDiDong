638fbf2a02e7672e0e2ff8c09db2d5315a01537c
nhatquang <142217823+pnwang1704@users.noreply.github.com>
2025-08-27 08:10:35 +0700

    Finish cau 8

---
+++ tuan02/dist/promisechain.js
@@ -0,0 +1,8 @@
+"use strict";
+Promise.resolve(2)
+    .then(num => num * num)
+    .then(num => num * 2)
+    .then(num => num + 5)
+    .then(result => {
+    console.log("Final result:", result);
+});
