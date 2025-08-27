53cfbe899047c429630c97e6dfb0cb9fb3825b22
nhatquang <142217823+pnwang1704@users.noreply.github.com>
2025-08-27 08:15:36 +0700

    Finish cau 9

---
+++ tuan02/src/promisefilternumber.ts
@@ -0,0 +1,12 @@
+function filterEvenNumbers(arr: number[]): Promise<number[]> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const evenNumbers = arr.filter((num) => num % 2 === 0);
+      resolve(evenNumbers);
+    }, 1000); 
+  });
+}
+
+filterEvenNumbers([1, 2, 3, 4, 5, 6]).then((result) => {
+  console.log("Even numbers:", result);
+});
