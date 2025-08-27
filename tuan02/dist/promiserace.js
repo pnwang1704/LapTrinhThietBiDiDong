41135e101af3b1425c452da6765b3a2fe8c6b07d
nhatquang <142217823+pnwang1704@users.noreply.github.com>
2025-08-27 08:08:13 +0700

    Finish cau 7

---
+++ tuan02/dist/promiserace.js
@@ -0,0 +1,15 @@
+"use strict";
+function simulateMultiTask1(time) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`Task done in ${time} ms`);
+        }, time);
+    });
+}
+Promise.race([
+    simulateMultiTask1(1000),
+    simulateMultiTask1(1500),
+    simulateMultiTask1(2000)
+]).then((result) => {
+    console.log("First completed task:", result);
+});
