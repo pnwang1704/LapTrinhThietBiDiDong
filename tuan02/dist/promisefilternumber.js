17401774b9aafe69cd2dce3900fef9c0fd212907
nhatquang <142217823+pnwang1704@users.noreply.github.com>
2025-08-27 08:31:14 +0700

    Finish cau 10

--- tuan02/dist/promisefilternumber.js
+++ tuan02/dist/promisefilternumber.js
@@ -1,12 +1,36 @@
 "use strict";
 function filterEvenNumbers(arr) {
-    return new Promise((resolve) => {
+    return new Promise((resolve, reject) => {
         setTimeout(() => {
-            const evenNumbers = arr.filter((num) => num % 2 === 0);
-            resolve(evenNumbers);
-        }, 1000);
+            if (arr.length === 0) {
+                reject("Array is empty!");
+            }
+            else {
+                const evenNumbers = arr.filter((num) => num % 2 === 0);
+                resolve(evenNumbers);
+            }
+        }, 1000); // 1 giây
     });
 }
-filterEvenNumbers([1, 2, 3, 4, 5, 6]).then((result) => {
+// Ví dụ chạy thành công
+filterEvenNumbers([1, 2, 3, 4, 5, 6])
+    .then((result) => {
     console.log("Even numbers:", result);
+})
+    .catch((error) => {
+    console.error("Error:", error);
+})
+    .finally(() => {
+    console.log("Done");
+});
+// Ví dụ chạy thất bại
+filterEvenNumbers([])
+    .then((result) => {
+    console.log("Even numbers:", result);
+})
+    .catch((error) => {
+    console.error("Error:", error);
+})
+    .finally(() => {
+    console.log("Done");
 });
